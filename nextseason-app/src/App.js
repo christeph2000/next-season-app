f82ee038d2ec08b07196306ce410eb6c8dc09f5c
Christeph P Emmanuvel <christephpemmanuvel200@gmail.com>
2023-11-01 16:13:14 +0530

    Removed inner git file

---
+++ nextseason-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "./Components/Title";
+
+function App() {
+  return (
+    <div className="App">
+      <Title />
+    </div>
+  );
+}
+
+export default App;
---
+++ nextseason-app/src/Components/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Title.css";
+
+export default function Title() {
+  return (
+    <div className="Title">
+      <h1>NextSeason</h1>
+    </div>
+  );
+}
